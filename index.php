22ac73e4eb8059963ef1a94efb5de6b75a782d64
jp <jp@example.com>
2022-03-13 08:23:49 +0100

    Modyfikacje w pliku index.php

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 	<h1>PHP Test</h1>
 		<p><b>An Example of PHP in Action</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
-		echo date("g:i A l, F j Y.");?> </p>
+		echo date("g:i A l, F j Y.");
+        echo "\nTo jest branch 0313_01";  ?> </p>
 	<h2>PHP Information</h2> 
 		<p> <?php phpinfo(); ?> </p> 
         <h2>GOEDEMIDDAG GEZELLIGE GRIEZELS UIT GRONINGEN</h2>
